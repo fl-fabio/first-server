6a3d09e32f17a8ad971009d08d850ea2b93ba333
fl-fabio <fl.fabio.lanzafame@gmail.com>
2023-06-19 18:07:11 +0200

    feature(): add validator for put and patch

---
+++ src/middleware/validation.ts
@@ -0,0 +1,62 @@
+import { body } from "express-validator";
+
+export const validatePostRequest = [
+  body("name")
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 4 })
+    .withMessage("Name must be at least 6 characters long"),
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid Email"),
+  body("username")
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 5 })
+    .withMessage("Name must be at least 6 characters long"),
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
+  body("phone")
+    .notEmpty()
+    .withMessage("Phone number is required")
+    .matches(/^\d{10}$/)
+    .withMessage("Invalid phone number format"),
+];
+
+export const validatePatchRequest = [
+  body("name")
+    .optional()
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 4 })
+    .withMessage("Name must be at least 6 characters long"),
+  body("email")
+    .optional()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid Email"),
+  body("username")
+    .optional()
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 5 })
+    .withMessage("Name must be at least 6 characters long"),
+  body("password")
+    .optional()
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
+  body("phone")
+    .optional()
+    .notEmpty()
+    .withMessage("Phone number is required")
+    .matches(/^\+\d{2}-\d{10}$/)
+    .withMessage("Invalid phone number format"),
+];
--- src/routes/usersApi.ts
+++ src/routes/usersApi.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {v4 as uuidV4} from 'uuid';
 import { addUserHandler, deleteUserHandler, getUserByIdHandler, getUsersHandler, updateUserHandler } from "../controllers/users";
+import {validatePatchRequest, validatePostRequest} from '../middleware/validation'
 
 import { User } from "../models/User";
 
@@ -10,8 +11,8 @@ router.get("/", getUsersHandler);
 
 router.get("/:id", getUserByIdHandler);
 
-router.post("/", addUserHandler);
+router.post("/", validatePostRequest, addUserHandler);
 
-router.patch("/:id", updateUserHandler);
+router.patch("/:id",validatePatchRequest, updateUserHandler);
 
 router.delete("/:id", deleteUserHandler);
